ce47fd0d892f117a25d1f6de7923a295e8ed2b2e
Yoann Katchourine <810667+ykatchou@users.noreply.github.com>
2026-03-23 22:50:06 +0100

    release version6 & 1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.ykatchou.ylauncher"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
